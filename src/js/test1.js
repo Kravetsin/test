7ecb4aab08ffaaef465b80a78f917304eb0578e6
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-08-19 23:53:10 +0200

    Summary

--- src/js/test1.js
+++ src/js/test1.js
@@ -1,27 +1,9 @@
-function multiplyMatrices(A, B) {
-  const n = A.length;
-  const C = new Array(n).fill(0).map(() => new Array(n).fill(0));
+let pricing = document.querySelector('.pricing__text');
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      for (let k = 0; k < n; k++) {
-        C[i][j] += A[i][k] * B[k][j];
-      }
-    }
-  }
+const setPricing = () => {
+  pricing = 'hello';
+};
 
-  return C;
-}
+setPricing();
 
-const A = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-const B = [
-  [10, 11, 12],
-  [13, 14, 15],
-  [16, 17, 18],
-];
-
-console.log(multiplyMatrices(A, B));
+console.log(pricing.textContent);
